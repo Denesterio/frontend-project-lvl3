b2ef9ffc73f20652c6da6afacb1876e942a46c4c
Denesterio <mrdnsong@gmail.com>
2021-06-06 12:07:40 +0300

    first preparations

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Denesterio/frontend-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/Denesterio/frontend-project-lvl3/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/e7b95493c7daa3b91ff7/maintainability)](https://codeclimate.com/github/Denesterio/frontend-project-lvl3/maintainability)
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/app/index.js',
+  output: {
+    path: __dirname + '/dist', // Folder to store generated bundle
+    filename: 'bundle.js', // Name of generated bundle after build
+    publicPath: '/', // public URL of the output directory when referenced in a browser
+  },
+  module: {
+    // where we defined file patterns and their loaders
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    // Array of plugins to apply to build chunk
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/public/index.html',
+      inject: 'body',
+    }),
+  ],
+  devServer: {
+    contentBase: './src/public', //source of static assets
+    port: 7700, // port to run dev-server
+  },
+};
